7c4e7e0cf649c24288b35f7551cb2a3787c1346c
Andrea <andrea.damico@gmail.com>
2011-01-28 14:53:41 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toxnotes::Application.config.secret_token = '2921e8fb97bd6d4d71350dc829b171ba468c5a82e807f7c646061720691a277363ec09015512c0847e49dac8968f647a2c4c20ecb1578eb01ac3af79231a434a'
