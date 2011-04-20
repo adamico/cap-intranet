31e0c942c05f0ded310ef5e40adef4b9aca4348d
Andrea <andrea.damico@gmail.com>
2011-04-20 10:42:05 +0200

    fixed helpers not loading for static pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_filter :authenticate_user!
   helper "event_calendar/calendar"
+  helper "refinery/helpers/meta"
+  helper "refinery/helpers/head"
+  helper "refinery/helpers/menu"
+  helper_method :refinery_user?
 end
